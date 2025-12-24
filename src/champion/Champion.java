454c82bf633043221895e4691cd8a3dda30c1b25
EUNSIK JUNG <bamtol2100@naver.com>
2025-12-24 12:47:51 +0900

    Champion추상 클래스 생성 및 메서드 공통/추상메서드 추가

---
+++ src/champion/Champion.java
@@ -0,0 +1,49 @@
+package champion;
+
+public abstract class Champion {
+
+    // 자식생성자에서 사용할수있는 protected 사용
+    protected String name;
+    protected int level;
+    protected int hp;
+    protected int attackDamage;
+    protected int defense;
+
+    // 생성자
+    public Champion(String name, int level, int hp, int attackDamage, int defense) {
+        this.name = name;
+        this.level = level;
+        this.hp = hp;
+        this.attackDamage = attackDamage;
+        this.defense = defense;
+    }
+
+    // 기본공격 공통 메서드
+    public void basicAttack(Champion target) {
+        System.out.println(name + "이(가) 기본 공격을 합니다.");
+        target.takeDamage(attackDamage);
+    }
+
+    // 공격받는 공통 메서드
+    public void takeDamage(int damage) {
+        int actualDamage = damage - defense;
+
+        if(actualDamage<0) actualDamage = 0;
+
+        hp -= actualDamage;
+        System.out.println(name + "이(가) " + actualDamage + "의 피해를 받았습니다.");
+        System.out.println("HP : " + hp);
+    }
+
+    // 레벨업 공통 메서드
+    public void levelUp() {
+        level++;
+    }
+
+    // 추상 메서드 추가
+    public abstract void useQ(Champion target);
+    public abstract void useW(Champion target);
+    public abstract void useE(Champion target);
+    public abstract void useR(Champion target);
+
+}
